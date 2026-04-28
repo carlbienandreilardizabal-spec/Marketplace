ca56139a5f531c796bf45725f273ac08585dec7c
carlbienandreilardizabal-spec <carlbienandreilardizabal@gmail.com>
2026-04-28 11:18:42 +0800

    fix again

--- src/app/product-detail/product-detail.page.ts
+++ src/app/product-detail/product-detail.page.ts
@@ -6,8 +6,10 @@ import { RouterModule } from '@angular/router';
 import { ApiService, Product } from '../services/api';
 import { addIcons } from 'ionicons';
 import { star, starOutline, cartOutline, arrowBackOutline } from 'ionicons/icons';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-product-detail',
   templateUrl: './product-detail.page.html',
   styleUrls: ['./product-detail.page.scss'],
